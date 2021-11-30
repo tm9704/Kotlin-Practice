fc75448b1dedf93f93a0dec31d7f8b7a1609cbc8
tm9704 <tm9704@korea.ac.kr>
2021-11-30 15:09:15 +0900

    Kotlin Basic Data Type

---
+++ KotlinProgramming/src/chapter02/section2/ValVar.kt
@@ -0,0 +1,12 @@
+package chapter02.section2
+
+fun main(language: String){
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+}
