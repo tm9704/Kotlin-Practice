181328a2ab00f2cf3f23a0fa934a2f33603b6f8b
tm9704 <tm9704@korea.ac.kr>
2021-12-04 15:10:17 +0900

    Kotlin Operator

---
+++ KotlinProgramming/src/chapter02/section4/UshrEx.kt
@@ -0,0 +1,11 @@
+package chapter02.section4
+
+fun main(){
+    val number1 = 5
+    val number2 = -5
+
+    println(number1 shr 1)
+    println(number1 ushr 1) //변화 x
+    println(number2 shr 1) //부호 비트 1로 유지
+    println(number2 ushr 1) //부호 비트가 0이 되면서 변경
+}
