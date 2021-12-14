34a464c8d306e2586b0487f32a5071eaab8d866f
tm9704 <tm9704@korea.ac.kr>
2021-12-14 14:32:52 +0900

    Kotlin Exception

---
+++ KotlinProgramming/src/chap04/section3/CustomExceptionTest.kt
@@ -0,0 +1,21 @@
+package chap04.section3
+
+class InvalidNameException(message: String) : Exception(message) // 1. 사용자 예외 클래스
+
+fun main() {
+    var name = "Kildong123" // 2. 숫자가 포함된 이름
+
+    try{
+        validateName(name)
+    }catch(e: InvalidNameException){//숫자가 포함된 예외처리
+        println(e.message)
+    }catch(e: Exception){ //기타 예외처리
+        println(e.message)
+    }
+}
+
+fun validateName(name: String){
+    if(name.matches(Regex(".*\\d+.*"))){ // 4. 이름에 숫자가 포함되어 있으면 예외를 발생시킴
+        throw InvalidNameException("Your name: $name : contains numerals.")
+    }
+}
---
+++ KotlinProgramming/src/chap04/section3/TryCatch.kt
@@ -0,0 +1,16 @@
+package chap04.section3
+
+fun main() {
+    val a = 6
+    val b = 0
+    val c : Int
+
+    try{
+        c = a/b // 0으로 나눔
+    }catch(e: /*ArithmeticException*/Exception) {
+        e.printStackTrace() //스택 추적
+//        println("Exception is handled")
+    }finally{
+        println("finally 블록은 반드시 항상 실행됨")
+    }
+}
