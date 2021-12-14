d7d17ba7446411272c03a19be610d8cdee0d58ff
tm9704 <tm9704@korea.ac.kr>
2021-12-14 12:37:47 +0900

    Kotlin IF

---
+++ KotlinProgramming/src/chap04/secton1/IfElseIfCondition.kt
@@ -0,0 +1,25 @@
+package chap04.secton1
+
+fun main() {
+    print("Enter the score: ")
+    val score = readLine()!!.toDouble()
+    var grade: Char = 'F'
+
+//    if(score >= 90.0){
+//        grade = 'A'
+//    }else if(score >= 80.0 && score <= 89.9){
+//        grade = 'B'
+//    }else if(score >= 70.0 && score <= 79.9){
+//        grade = 'C'
+//    }
+
+    if(score >= 90.0){
+        grade = 'A'
+    }else if (score in 80.0..89.9){
+        grade = 'B'
+    }else if (score in 70.0..79.9){
+        grade = 'C'
+    }
+
+    println("Score: $score, Grade: $grade")
+}
