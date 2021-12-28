8c53555e053b5604172da4e80331cbfc620eab52
tm9704 <tm9704@korea.ac.kr>
2021-12-28 11:27:56 +0900

    Kotlin Lambda One Paramater

--- KotlinProgramming/src/chap03/section3/ParamCount.kt
+++ KotlinProgramming/src/chap03/section3/ParamCount.kt
@@ -3,6 +3,14 @@ package chap03.section3
 fun main() {
     noParam({"Hello World!"})
     noParam{"Hello World!"}
+
+    oneParam({a->"Hello World! $a"})
+    oneParam{a->"Hello World! $a"}
+    oneParam{"Hello World! $it"}
 }
 
 fun noParam(out: () -> String) = println(out())
+
+fun oneParam(out: (String) -> String){
+    println(out("OneParam"))
+}
