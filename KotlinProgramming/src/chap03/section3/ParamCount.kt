5cf15bcc2e6a9978a4bd755a02afda4346877e43
tm9704 <tm9704@korea.ac.kr>
2021-12-28 11:47:22 +0900

    Kotlin Normal Param & Lambda Param

--- KotlinProgramming/src/chap03/section3/ParamCount.kt
+++ KotlinProgramming/src/chap03/section3/ParamCount.kt
@@ -9,6 +9,10 @@ fun main() {
     oneParam{"Hello World! $it"}
 
     moreParam{a,b -> "Hello World! $a $b"} // 매개변수 이름 생략 불가
+
+    withArgs("Args1", "Args2", {a,b-> "Hello World $a $b"})
+
+    withArgs("Args1", "Args2"){a,b -> "Hello World $a, $b"}
 }
 
 fun noParam(out: () -> String) = println(out())
@@ -19,4 +23,8 @@ fun oneParam(out: (String) -> String){
 
 fun moreParam(out: (String, String) -> String){
     println(out("OneParam", "TwoParam"))
+}
+
+fun withArgs(a:String, b: String, out: (String, String) -> String){
+    println(out(a,b))
 }
