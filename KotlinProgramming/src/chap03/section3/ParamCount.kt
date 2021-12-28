6ee9d42fe4cab8af936174913408a1801c84e38a
tm9704 <tm9704@korea.ac.kr>
2021-12-28 11:37:17 +0900

    Kotlin Lambda More Paramater

--- KotlinProgramming/src/chap03/section3/ParamCount.kt
+++ KotlinProgramming/src/chap03/section3/ParamCount.kt
@@ -7,10 +7,16 @@ fun main() {
     oneParam({a->"Hello World! $a"})
     oneParam{a->"Hello World! $a"}
     oneParam{"Hello World! $it"}
+
+    moreParam{a,b -> "Hello World! $a $b"} // 매개변수 이름 생략 불가
 }
 
 fun noParam(out: () -> String) = println(out())
 
 fun oneParam(out: (String) -> String){
     println(out("OneParam"))
+}
+
+fun moreParam(out: (String, String) -> String){
+    println(out("OneParam", "TwoParam"))
 }
